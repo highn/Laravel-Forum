817706052d6893cd9b3aca4d98843e650f83ab52
Highn Hymn <highn@hotmail.com>
2019-09-28 16:34:02 +0500

    Relationships

---
+++ app/Model/Reply.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+use App\User;
+
+class Reply extends Model
+{
+    //
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
