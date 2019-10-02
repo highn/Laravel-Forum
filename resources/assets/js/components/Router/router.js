7ca3c0e2a0eaaad558ba40132889af62cc4ef6f2
Highn Hymn <highn@hotmail.com>
2019-10-02 21:45:02 +0500

    User Login

---
+++ resources/assets/js/components/Router/router.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+import Login from '../login/Login'
+const routes = [
+    //{ path: '/', component: Home },
+    { path: '/login', component: Login }
+  ]
+
+
+const router = new VueRouter({
+    routes, // short for `routes: routes`
+    hashbang: false,
+    mode: 'history'
+})
+
+
+export default router
--- routes/api.php
+++ routes/api.php
@@ -15,11 +15,8 @@
     return $request->user();
 }); */
 Route::apiResource('/question', 'QuestionController');
-
 Route::apiResource('/category', 'CategoryController');
-
 Route::apiResource('/question/{question}/reply', 'ReplyController');
-
 Route::post('/like/{reply}', 'LikeController@likeIt');
 Route::delete('/like/{reply}', 'LikeController@unlikeIt');
 
